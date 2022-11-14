aee322db4d862a99e375fa7a03862848b24ab62b
Joey Huab <joey@evolution-x.org>
2022-11-14 07:51:03 +0000

    sm8350-common: Switch to GCamGOPrebuilt-V3

--- common.mk
+++ common.mk
@@ -130,7 +130,7 @@ PRODUCT_PACKAGES_DEBUG += \
 # Camera
 PRODUCT_PACKAGES += \
     libgui_vendor \
-    Aperture
+    GCamGOPrebuilt-V3
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
